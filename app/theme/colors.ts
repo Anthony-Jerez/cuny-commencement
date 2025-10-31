6978464a3885c771ba5b0ab940d8bd9aede94fbf
Anthony Jerez <ajerez450@gmail.com>
2025-10-30 22:47:59 -0400

    feat(theme): add QC app color theme

---
+++ app/theme/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  text: "#222",
+  muted: "#666",
+  border: "#eee",
+  cardBg: "#fff",
+
+  qcRed: "#c44536",
+  qcRedDark: "#b33e30",
+  qcPink: "#ffd9d6",
+  qcPinkBorder: "#f2b0ad",
+  qcBlushBg: "#fff5f4",
+
+  userPill: "#d9534f",
+};
